b98a791c599e46183c122265b4362a16534329f1
fadywadea <fwadea3@gmail.com>
2023-12-27 20:22:52 +0200

    update the code

--- database/dbConnection.js
+++ database/dbConnection.js
@@ -5,5 +5,5 @@ import mongoose from 'mongoose';
 export const mongooseConnection = mongoose.connect('mongodb://127.0.0.1:27017/test').then(() => {
   console.log("Database connected");
 }).catch((e) => {
-  console.log("Database error");
+  console.log("Database error:", e);
 });
